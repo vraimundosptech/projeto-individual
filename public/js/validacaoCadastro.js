c8d3446cb97a987f0a6d880d1c44ff60d406258c
Vitor Machado <vitor.raimundo@sptech.school>
2026-04-10 21:11:30 -0300

    feat: adicionando data-web-viz ao repositório

---
+++ public/js/validacaoCadastro.js
@@ -0,0 +1,29 @@
+/* function validar() {
+    let codigo = codigo_input.value;
+    let nome = nome_input.value;
+    let cpf = cpf_input.value;
+    let email = email_input.value;
+    let senha = senha_input.value;
+    let confirmacao_senha = confirmacao_senha_input.value;
+
+    if (nome.length <= 1) {
+        cardErro.style.display = 'block';
+        mensagem_erro.innerHTML = 'Nome muito curto!';
+    }
+    if (!email.includes('@')) {
+        cardErro.style.display = 'block';
+        mensagem_erro.innerHTML = 'Email não contêm "@"!';
+    }
+    if (senha.length <= 6) {
+        cardErro.style.display = 'block';
+        mensagem_erro.innerHTML = 'Senha muito curta!';
+    }
+    if (senha != confirmacao_senha) {
+        cardErro.style.display = 'block';
+        mensagem_erro.innerHTML = 'Senhas incopatíveis';
+    }
+    if (cpf.length != 11) {
+        cardErro.style.display = 'block';
+        mensagem_erro.innerHTML = 'CPF deve ter 11 caracteres!';
+    }
+} */

d2a4d479a61a423f4a1ed37c96a715bd01ee2d59
Vitor Machado <vitor.raimundo@sptech.school>
2026-05-23 22:24:01 -0300

    feat: adicionando função de sessão nas páginas do usuário

--- public/js/sessao.js
+++ public/js/sessao.js
@@ -4,13 +4,7 @@ function validarSessao() {
     var nome = sessionStorage.NOME_USUARIO;
     var id = sessionStorage.ID_USUARIO;
 
-    var b_usuario = document.getElementById("b_usuario");
-
-    if (email != null && nome != null && id != null) {
-        if (b_usuario) {
-            b_usuario.innerHTML = nome;
-        }
-    } else {
+    if (email == null || nome == null || id == null) {
         window.location = "../login.html";
     }
 }
@@ -19,21 +13,3 @@ function limparSessao() {
     sessionStorage.clear();
     window.location = "../login.html";
 }
-
-// carregamento (loading)
-function aguardar() {
-    var divAguardar = document.getElementById("div_aguardar");
-    divAguardar.style.display = "flex";
-}
-
-function finalizarAguardar(texto) {
-    var divAguardar = document.getElementById("div_aguardar");
-    divAguardar.style.display = "none";
-
-    var divErrosLogin = document.getElementById("div_erros_login");
-    if (texto) {
-        divErrosLogin.style.display = "flex";
-        divErrosLogin.innerHTML = texto;
-    }
-}
-
